7158016516970b42d9d71fe311d1157eed4ca350
dpdltmxkdnj <dpdltmxkdnj@naver.com>
2024-01-17 23:53:57 +0900

    home controller getmapping

--- src/main/java/mybatis_db/board/controller/HomeController.java
+++ src/main/java/mybatis_db/board/controller/HomeController.java
@@ -1,11 +1,12 @@
 package mybatis_db.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping
+    @GetMapping
     public String home() {
         return "redirect:/home";
     }
